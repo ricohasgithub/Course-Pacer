43b7be6773db6bd1ef8823127ed210028ca0217f
Rico Zhu <ricozhuthegreat@hotmail.com>
2021-12-19 10:31:32 -0500

    Patched Bug with Authentication Form Redirect

--- public/src/js/login.js
+++ public/src/js/login.js
@@ -1,35 +1,43 @@
 
-function signup () {
+document.getElementById("signup-button").onclick = function(e) {
 
     // Retrieve the email and password information off the index.html page
     let username = document.querySelector('#username').value;
     let password = document.querySelector('#password').value;
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log("Clicked");
 
     firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
     })
+  
+};
 
-}
-
-function login() {
+document.getElementById("login-button").onclick = function(e) {
 
     // Retrieve the email and password information off the index.html page
     let username = document.querySelector('#username').value;
     let password = document.querySelector('#password').value;
 
+    e.preventDefault();
+    e.stopPropagation();
+
     firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
     })
-    
-}
+
+};
 
 // Firebase listener for html page redirection
 firebase.auth().onAuthStateChanged(function(user) {
